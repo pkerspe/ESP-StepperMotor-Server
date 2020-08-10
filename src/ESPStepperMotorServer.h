f18a4fa9d32e020922a75ffaac42dea5330af16a
Paul Kerspe <paul@kerspe.com>
2020-08-10 19:19:24 +0200

    bumpred version to 0.4.0

--- src/ESPStepperMotorServer.h
+++ src/ESPStepperMotorServer.h
@@ -204,7 +204,7 @@ private:
   // private member variables
   //
   byte enabledServices;
-  const char *version = "0.3.3";
+  const char *version = "0.4.0";
   boolean isWebserverEnabled = false;
   boolean isRestApiEnabled = false;
   boolean isCLIEnabled = false;
